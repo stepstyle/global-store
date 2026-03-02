0cabaca5ad4c314aed7f02c2337486dd580de489
mohammed antari <mohmmedmostakl@gmail.com>
2026-03-02 20:24:46 +0300

    Fix mobile card clipping and image render issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ import { useCart } from '../App';
 
 const Footer: React.FC = () => {
   const { t } = useCart();
+  
   return (
     <footer className="bg-slate-900 text-slate-300 pt-0 pb-8 font-sans relative">
       {/* Theme Gradient Strip */}
